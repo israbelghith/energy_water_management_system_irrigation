be0f36d5bff251f7025aa2d9a1e549c247fd30cf
israa790 <belghitisraa@gmail.com>
2025-12-26 14:16:44 +0100

    fix: Add FetchType.EAGER to resolve lazy loading issues

--- energy_service/src/main/java/tn/isra/belghith/entities/ConsommationElectrique.java
+++ energy_service/src/main/java/tn/isra/belghith/entities/ConsommationElectrique.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,26 +20,23 @@ public class ConsommationElectrique {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @ManyToOne
-	@JoinColumn(name = "pompe_id")
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "pompe_id")
     private Pompe pompe;
-    
-    
+
     @Column(nullable = false)
     private Double energieUtilisee; // en kWh
-    
+
     @Column(nullable = false)
     private Double duree; // en heures
-    
+
     @Column(nullable = false)
     private Date dateMesure;
-    
+
     /*
      * @Transient
      * private boolean surconsommation;
      */
-    
-    
-    
+
 }
--- energy_service/src/main/java/tn/isra/belghith/entities/Pompe.java
+++ energy_service/src/main/java/tn/isra/belghith/entities/Pompe.java
@@ -1,7 +1,5 @@
 package tn.isra.belghith.entities;
 
-
-
 import jakarta.persistence.*;
 
 import lombok.Data;
@@ -11,28 +9,26 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-
 @Entity
 @Data
 public class Pompe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(nullable = false, unique = true)
     private String reference;
-    
-    @Column(nullable = false)
-    private Double puissance;  // en kW
-    
-   @Enumerated(EnumType.STRING)
-    private StatutPompe statut;
-    
-   @Column(nullable = false)
-    private Date dateMiseEnService;
-    
-    @OneToMany(mappedBy = "pompe", cascade = CascadeType.PERSIST)
-	@JsonIgnore
-	private List<ConsommationElectrique> listeConsommationsElectriques;
-}
 
+    @Column(nullable = false)
+    private Double puissance; // en kW
+
+    @Enumerated(EnumType.STRING)
+    private StatutPompe statut;
+
+    @Column(nullable = false)
+    private Date dateMiseEnService;
+
+    @OneToMany(mappedBy = "pompe", cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<ConsommationElectrique> listeConsommationsElectriques;
+}
