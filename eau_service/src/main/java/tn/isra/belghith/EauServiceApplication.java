14d2de4a48591cba7ded3c488c52b593a9148c70
israa790 <belghitisraa@gmail.com>
2025-12-26 14:16:37 +0100

    feat: Add RabbitMQ integration with @EnableRabbit

--- eau_service/src/main/java/tn/isra/belghith/EauServiceApplication.java
+++ eau_service/src/main/java/tn/isra/belghith/EauServiceApplication.java
@@ -1,5 +1,6 @@
 package tn.isra.belghith;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableRabbit
 public class EauServiceApplication {
 
 	public static void main(String[] args) {
